e0f58c000cbbeffce14b84b6b6c0b25c814a451b
ahmadalmuhtar <ahmadalmuhtar@gmail.com>
2024-02-23 23:24:29 +0100

    practiced the Ref focus function

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,25 @@
-import Image from "next/image";
+"use client";
+
+import Form from "@/components/Form";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
-  return <main></main>;
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+
+  return (
+    <>
+      <main className="flex justify-center mt-5 space-x-96 w-full max-w-screen-lg">
+        <Form />
+        <Form focusRef={inputRef} />
+        <Form />
+        <Form />
+      </main>
+    </>
+  );
 }
