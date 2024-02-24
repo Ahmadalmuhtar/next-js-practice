67867d67f6c5947f1abf9918312a017419f8a825
ahmadalmuhtar <ahmadalmuhtar@gmail.com>
2024-02-24 22:45:32 +0100

    created a new component with TypeScript and added it to its route

---
+++ pages/blog/post/index.tsx
@@ -0,0 +1,12 @@
+import PostsList from "@/components/PostsList";
+import React from "react";
+
+const Post = () => {
+  return (
+    <>
+      <PostsList />
+    </>
+  );
+};
+
+export default Post;
